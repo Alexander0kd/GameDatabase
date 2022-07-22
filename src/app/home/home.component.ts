f3d646d54ab0b2359259b8a244f3cedce0d75f6c
Buzz1233 <kademskyi.alexander@gmail.com>
2022-07-22 20:44:54 +0300

    Add game cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeComponent implements OnInit {
 
+  public sort: string = 'name';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public tmp: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 }
